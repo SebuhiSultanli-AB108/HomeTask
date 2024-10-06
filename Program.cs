046ee3b3b3123fb2e0caa8dd11158b41f7ba7d6d
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-06 21:26:50 +0400

    Array_HomeTask_1.0

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿#region LabTask1
+/*---> İki ədəd verilir. Birinci ədədin ikinci ədəd qüvvətini hesablayan alqoritmin diaqramını qurun. <---*/
+
+//int x = int.Parse(Console.ReadLine());
+//int y = int.Parse(Console.ReadLine());
+
+//int num = x;
+
+//for (int i = 1; i < y; i++)
+//{
+//    num *= x;
+//}
+
+//Console.WriteLine(num);
+
+#endregion
+
+#region LabTask2
+/*---> Verilmiş ədədin neçə mərtəbəli olduğunu print edən alqoritmin diaqramını qurun. <---*/
+
+//int num = int.Parse(Console.ReadLine());
+//int count = 0;
+
+//while(num > 0)
+//{
+//    num /= 10;
+//    count++;
+//}
+
+//Console.WriteLine(count);
+
+#endregion
+
+
+#region LabTask3
+/*---> Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin (Məs: { 1,77,6,14} arrayında 2 rəqəm var ) <---*/
+
+//int[] nums = { 1, 77, 6, 14, 56, 8, 2 };
+//int count = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] < 10 && nums[i] > 0) count++;
+//}
+
+//Console.WriteLine(count);
+
+#endregion
