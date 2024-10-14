d4340ec8895e3ce7a01c1c1e40e23c7ee2a6e8c6
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-14 21:34:50 +0400

    Classes

---
+++ Classes/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    public class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+
+        public Product(string name, string brandName, double price)
+        {
+            Name = name;
+            BrandName = brandName;
+            Price = price;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Product[] Productlist = 
+            {
+                new Product("Ally", "ROG", 622.99),
+                new Product("SteamDeck", "Steam", 739.99),
+                new Product("Legion", "Lenovo", 1889.99),
+                new Product("Aspire", "Acer", 799.99),
+                new Product("TUF", "ASUS", 999.99),
+            };
+
+            double min = 750;
+            double max = 1000;
+
+            ShowItemPriceAtRange(Productlist, min, max);
+
+            static void ShowItemPriceAtRange(Product[] productList, double minPrice, double maxPrice)
+            {
+                foreach (Product product in productList)
+                {
+                    if (product.Price < maxPrice && product.Price > minPrice)
+                    {
+                        Console.WriteLine("---------------------------");
+                        Console.WriteLine($"Product name: {product.Name}");
+                        Console.WriteLine($"Product brand: {product.BrandName}");
+                        Console.WriteLine($"Price: {product.Price}");
+                        Console.WriteLine("---------------------------");
+                    }
+                }
+            }
+        }
+    }
+}
