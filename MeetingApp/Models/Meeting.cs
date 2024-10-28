da57a5b8fb6613999f611bdd13008fdaf6a67013
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-28 22:40:21 +0400

    metting app

---
+++ MeetingApp/Models/Meeting.cs
@@ -0,0 +1,59 @@
+﻿namespace MeetingApp.Models;
+
+public class Meeting
+{
+    public string FullName { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+
+    public Meeting(string fullName, DateTime fromDate, DateTime toDate)
+    {
+        FullName = fullName;
+        FromDate = fromDate;
+        ToDate = toDate;
+
+
+    }
+}
+
+
+public class ReservedDateInterval : Exception
+{
+    public ReservedDateInterval() : base("ReservedDateInterval") { }
+    public ReservedDateInterval(string message) : base(message) { }
+}
+
+public class WrongDateInterval : Exception
+{
+    public WrongDateInterval() : base("WrongDateInterval") { }
+    public WrongDateInterval(string message) : base(message) { }
+}
+
+public class MeetingSchedule
+{
+    public Meeting[] Meetings = new Meeting[0];
+
+    public void SetMeeting(string fullName, DateTime fromDate, DateTime toDate)
+    {
+        foreach (var item in Meetings)
+        {
+            if ((item.FromDate > fromDate && item.ToDate < fromDate) || (item.FromDate > toDate && item.ToDate < toDate))
+            {
+                throw new ReservedDateInterval();
+            }
+        }
+        if (fromDate > toDate)
+        {
+            throw new WrongDateInterval();
+        }
+
+        Meeting newMeeting = new Meeting(fullName, fromDate, toDate);
+        Meeting[] newArr = new Meeting[Meetings.Length + 1];
+        for (int i = 0; i < Meetings.Length; i++)
+        {
+            newArr[i] = Meetings[i];
+        }
+        newArr[newArr.Length - 1] = newMeeting;
+        Meetings = newArr;
+    }
+}
---
+++ MeetingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using MeetingApp.Models;
+
+namespace MeetingApp;
+
+internal class Program
+{
+    static void Main()
+    {
+        MeetingSchedule MS = new MeetingSchedule();
+
+        try
+        {
+            MS.SetMeeting("MyMeeting1", DateTime.Today, DateTime.Today.AddHours(1));
+            MS.SetMeeting("MyMeeting2", DateTime.Today, DateTime.Today.AddHours(2));
+            MS.SetMeeting("MyMeeting3", DateTime.Today.AddHours(5), DateTime.Today.AddHours(3));
+        }
+        catch (ReservedDateInterval)
+        {
+            Console.WriteLine("ReservedDateInterval");
+        }
+        catch (WrongDateInterval)
+        {
+            Console.WriteLine("WrongDateInterval");
+        }
+
+
+        foreach (var item in MS.Meetings)
+        {
+            Console.WriteLine(item.FullName);
+        }
+    }
+}
