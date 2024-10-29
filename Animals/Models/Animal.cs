f6c36e8c686eb8e715ec7ab97c3ceea978b9e7df
Sabuhi <sabuhies-ap118@code.edu.az>
2024-10-29 21:09:26 +0400

    Animal aded

---
+++ Animals/Models/Animal.cs
@@ -0,0 +1,107 @@
+﻿namespace Animals.Models;
+
+abstract class Animal
+{
+    public int AvgLifeTime { get; set; }
+    public string Breed { get; set; }
+    public int HP { get; set; }
+    public Gender AnimalGender { get; set; }
+    public enum Gender : byte
+    {
+        Male = 1,
+        Female = 2
+    }
+}
+
+
+class Wolf : Animal
+{
+    public bool IsPrideLeader { get; set; }
+    public int AttackDamage { get; set; }
+
+    public void Hunt<T>(Animal animal)
+    {
+        animal.HP -= AttackDamage;
+    }
+}
+
+class Elephant : Animal
+{
+    public double Weight { get; set; }
+    public bool IsTrained { get; set; }
+}
+
+abstract class Food
+{
+    public int Calorie { get; set; }
+}
+
+class Meat : Food
+{
+    public Type MeatType { get; set; }
+    public enum Type : byte
+    {
+        Beef = 1,
+        Chicken = 2,
+        Pork = 3
+    }
+}
+
+class Grass : Food
+{
+    public string Name { get; set; }
+}
+
+class ZooCage<T, U>
+    where T : Animal
+    where U : Food
+{
+    T[] Animals = [];
+    U[] Foods = [];
+
+    public void Add(T animal)
+    {
+        Array.Resize(ref Animals, Animals.Length + 1);
+        Animals[^1] = animal;
+    }
+    public void Add(U food)
+    {
+        Array.Resize(ref Foods, Foods.Length + 1);
+        Foods[^1] = food;
+    }
+    public void PrintInfo()
+    {
+        Console.WriteLine("0 - Info for Animals\n" + "1 - Info for Food");
+        int input = Convert.ToInt32(Console.ReadLine());
+        bool printInfoAnimal = false;
+        switch (input)
+        {
+            case 0:
+                printInfoAnimal = true;
+                break;
+            case 1:
+                printInfoAnimal = false;
+                break;
+            default:
+                Console.WriteLine("Invalid input!");
+                Console.WriteLine("0 - Info for Animals\n" + "1 - Info for Food");
+                break;
+        }
+
+        if (printInfoAnimal)
+        {
+            foreach (var animal in Animals)
+            {
+                Console.WriteLine("====================================");
+                Console.WriteLine($"Breed: {animal.Breed}\nAnimal Gender: {animal.AnimalGender}\nHP: {animal.HP}");
+            }
+            Console.WriteLine("====================================");
+        }
+        else
+            foreach (var food in Foods)
+            {
+                Console.WriteLine("Calorie:" + food.Calorie);
+            }
+
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,72 @@
+﻿using Animals.Models;
+
+namespace Animals;
+
+internal class Program
+{
+    static void Main()
+    {
+        Wolf alfaWolf = new Wolf()
+        {
+            AttackDamage = 25,
+            AvgLifeTime = 35,
+            Breed = "GrayWolf",
+            HP = 100,
+            IsPrideLeader = true,
+            AnimalGender = Animal.Gender.Male,
+        };
+
+        Wolf betaWolf = new Wolf()
+        {
+            AttackDamage = 10,
+            AvgLifeTime = 45,
+            Breed = "GrayWolf",
+            HP = 60,
+            IsPrideLeader = false,
+            AnimalGender = Animal.Gender.Female,
+        };
+        Wolf sigmaWolf = new Wolf()
+        {
+            AttackDamage = 40,
+            AvgLifeTime = 25,
+            Breed = "Andrew Tate",
+            HP = 70,
+            IsPrideLeader = false,
+            AnimalGender = Animal.Gender.Male,
+        };
+        Elephant elephant1 = new Elephant()
+        {
+            HP = 100,
+            IsTrained = true,
+            Weight = 100,
+            AnimalGender = Animal.Gender.Male,
+        };
+        Elephant elephant2 = new Elephant()
+        {
+            HP = 70,
+            IsTrained = false,
+            Weight = 200,
+            AnimalGender = Animal.Gender.Female,
+        };
+        Meat meat1 = new Meat() { Calorie = 2000, MeatType = Meat.Type.Beef };
+        Meat meat2 = new Meat() { Calorie = 2500, MeatType = Meat.Type.Pork };
+
+        Grass grass1 = new Grass() { Calorie = 1000, Name = "LongGrass" };
+        Grass grass2 = new Grass() { Calorie = 700, Name = "ShortGrass" };
+
+        ZooCage<Animal, Food> Cage = new ZooCage<Animal, Food>();
+        Cage.Add(alfaWolf);
+        Cage.Add(betaWolf);
+        Cage.Add(sigmaWolf);
+        Cage.Add(elephant1);
+        Cage.Add(elephant2);
+        Cage.Add(meat1);
+        Cage.Add(meat2);
+        Cage.Add(grass1);
+        Cage.Add(grass2);
+
+        Cage.PrintInfo();
+        sigmaWolf.Hunt<Animal>(elephant1);
+        Cage.PrintInfo();
+    }
+}
