21ce3521ee061e67010abd2c4105b8c5d4153e81
Sabuhi <sabuhies-ap118@code.edu.az>
2024-11-03 18:49:31 +0400

    FileStream

---
+++ FileStream/Models/FileManager.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+
+namespace FileStream.Models;
+
+class FileManager
+{
+    string link = string.Empty;
+    public FileManager(string link)
+    {
+        this.link = link;
+    }
+    List<string> strList = new();
+    public void Add(string name)
+    {
+        Read(strList);
+        if (!strList.Contains(name))
+            strList.Add(name);
+        Write(strList);
+    }
+    public void Update(int index, string name)
+    {
+        Read(strList);
+        try
+        {
+            strList[index] = name;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Cant find what u are looking for mate!");
+        }
+        Write(strList);
+    }
+    public void Delete(int index)
+    {
+        Read(strList);
+        try
+        {
+            strList.RemoveAt(index);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Cant find what u are looking for mate!");
+        }
+        Write(strList);
+    }
+    private void Read(object? obj)
+    {
+        using StreamReader reader = new StreamReader(link);
+        List<string> strArr = JsonConvert.DeserializeObject<List<string>>(reader.ReadToEnd());
+        obj = strArr;
+    }
+    private void Write(object? obj)
+    {
+        using StreamWriter writer = new StreamWriter(link, false);
+        writer.WriteLine(JsonConvert.SerializeObject(obj));
+    }
+
+
+    /*
+    List<string> yaradırsınız. Bunu names.json adlı jsona yazırsınız.
+    void Add(string name)
+    bool Exist(stiring name)
+    void Update(int index, string name)
+    void Delete(int index)
+    Metodlarını adlarına uyğun olaraq yazın
+    */
+}
---
+++ FileStream/Program.cs
@@ -0,0 +1,16 @@
+﻿using FileStream.Models;
+
+namespace FileStream;
+class Program
+{
+    static void Main()
+    {
+        FileManager fm = new FileManager(@"C:\Users\Phoenix\Desktop\HomeTask\FileStream\Json\Data.json");
+        fm.Add("Sebuhi");
+        fm.Add("HumanA");
+        fm.Add("HumanB");
+        fm.Add("HumanC");
+        fm.Update(2, "HumanD");
+        fm.Delete(3);
+    }
+}
